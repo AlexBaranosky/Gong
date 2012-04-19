02a6880a31de1948bfcce54ac122d5a2ece5b8a1
Alex Baranosky <alexander.baranosky@gmail.com>
2012-04-18 23:13:27 -0400

    removed unneeded imports

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject gong "0.0.1"
   :description "Practice routine automation"
   :dependencies [[org.clojure/clojure "1.4.0-beta7"]
-                 [org.mobicents.external.freetts/freetts "1.0"]
                  [de.huxhorn.sulky/de.huxhorn.sulky.3rdparty.jlayer "1.0"]
                  [facts/speech-synthesis "1.0.0"]]
   :main gong.core)
--- src/gong/core.clj
+++ src/gong/core.clj
@@ -1,12 +1,9 @@
 (ns gong.core
   (:require [speech-synthesis.say :refer [say]])
-  (:import [java.applet Applet]
-           [java.io File]
-           [java.net URL]
-           [com.sun.speech.freetts VoiceManager]
+  (:import [java.io File]
            [javazoom.jl.player Player]))
 
-(defn play-wav [file-name]
+(defn- play-wav [file-name]
   (when-not (.exists (File. file-name))
     (throw (Exception. (str "Couldn't find wav file: " file-name))))
 
@@ -20,27 +17,29 @@
 (def ^:private secs-per-hour 3600)
 (def ^:private millis-per-sec 1000)
 
-(defn- parse-secs-from
+(defn- parse-millis-from
   "seconds from string such as \"00:05:00\" ;=> (300 seconds)"
   [string]
   (let [[_ hours mins secs] (re-matches #"^(\d\d):(\d\d):(\d\d)$" string)]
     (when-not hours
       (throw (Exception. (format "Duration, %s, was not formatted correctly." string))))
     (let [[hours mins secs] (map #(Integer/parseInt %) [hours mins secs])]
-      (+ secs
-        (* mins secs-per-min)
-        (* hours secs-per-hour)))))
+      (* millis-per-sec (+ secs
+                           (* mins secs-per-min)
+                           (* hours secs-per-hour))))))
 
 (defn- exec-practice-routine
   "practice-routine-sections looks like:
    [ {:duration \"00:01:00\" :message \"do something\"} {...} ...]"
   [practice-routine-sections]
+
   (println "** Routine Summary **")
   (doseq [{:keys [duration message]} practice-routine-sections] 
     (println (str duration " " message)))
+
   (doseq [{:keys [duration message]} practice-routine-sections]
     (say message)
-    (Thread/sleep (* millis-per-sec (parse-secs-from duration)))
+    (Thread/sleep (parse-millis-from duration))
     (play-wav "blong2.wav")))
 
 (defn -main []
